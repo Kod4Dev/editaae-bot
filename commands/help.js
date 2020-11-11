8dc04fb1c18a9004b5717960faba1aa074c54396
Kod4 <arthureichenberg8@gmail.com>
2020-11-11 06:35:42 -0300

    Ajustes no comando Help

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,5 @@
-const discord = require("discord.js")
+const discord = require("discord.js");
+const { validateID } = require("ytdl-core-discord");
 
 const help = new discord.MessageEmbed()
     .setColor('#FF00FF')
@@ -9,9 +10,7 @@ const help = new discord.MessageEmbed()
     .setImage('https://media.giphy.com/media/XH2SQ8gde4v4zLTtue/giphy.gif')
     .setFooter('Edita Aê!• Store | Todos os direitos reservados', 'https://i.ibb.co/gdWfTSx/logo.png')
     .addFields(
-        {name: `${process.env.PREFIX}play`, value: `Para tocar músicas`, inline: true},
-        {name: `${process.env.PREFIX}pause`, value: `Para pauser as músicas`, inline: true},
-        {name: `${process.env.PREFIX}skip`, value: `Para ir para a próxima música`, inline: true}
+        {name:`${process.env.TOKEN}sugestao`, value: "Para dar a sua sugestão no canal <#776013841246322708>"}
 
     )
 
